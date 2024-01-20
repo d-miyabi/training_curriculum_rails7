1702e0f671207c0490a4329dfc223f6095977556
d-miyabi <msgh3110+works@gmail.com>
2024-01-20 13:00:54 +0900

    Issue6

--- db/migrate/20230603133754_create_plans.rb
+++ db/migrate/20230603133754_create_plans.rb
@@ -1,6 +1,7 @@
 class CreatePlans < ActiveRecord::Migration[7.0]
   def change
     create_table :plans do |t|
+      t.string :calendars
       t.string :plan
       t.date :date
       t.timestamps
